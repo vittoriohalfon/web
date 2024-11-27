72b37c5323aee00c3a6031e8975fdd306d442d5c
Vittorio <vittoriohalfon01@gmail.com>
2024-11-27 19:35:45 +0200

    fix: buildspec - remove unused react markdown lib

--- components/shared/TenderCard.tsx
+++ components/shared/TenderCard.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import { useState } from 'react';
-import ReactMarkdown from 'react-markdown';
-import rehypeRaw from 'rehype-raw';
 
 interface TenderCardProps {
   tender: {
